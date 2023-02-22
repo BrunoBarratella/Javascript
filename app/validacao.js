d5df2c00fd496ef447f93a1405a8164f6a789fe2
Bruno Barratella <brubarratella@hotmail.com>
2023-02-21 22:32:45 -0300

    Projeto Semi-Pronto

--- app/validacao.js
+++ app/validacao.js
@@ -3,10 +3,12 @@ function verificaSeOChutePossuiUmValorValido(chute){
 
     if(chuteForInvalido(numero)){
         elementoChute.innerHTML +='<div>Valor inválido</div>'
+        return
     }
 
     if (numeroMaiorOuMenorQueOValorPermitido(numero)){
         elementoChute.innerHTML +=`<div>Valor inválido: o Numero secreto precisa estar entre ${menorValor} e ${maiorValor}</div>`
+        return
     }
 
     if (numero === numeroSecreto){
@@ -14,6 +16,10 @@ function verificaSeOChutePossuiUmValorValido(chute){
             <h2>Voce Acertou!</h2>
             <h3>O numero secreto era ${numeroSecreto}</h3>
         `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O Numero secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>`
+    }else{
+        elementoChute.innerHTML += `<div>O Numero secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>`
     }
 }
 
